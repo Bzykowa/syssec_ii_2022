164cf63e960b2dd18d3a46b5f797ccba17eaeb12
Bzykowa <karcia956@gmail.com>
2023-01-07 00:17:23 +0100

    BLS

---
+++ BLS/signer.py
@@ -0,0 +1,42 @@
+from mcl import G1, G2, Fr
+import sys
+from common_protocol import Initiator
+from klib import jstore
+
+sys.path.insert(1, "/home/karolina/mcl-python")
+group_init = b"bls_ss"
+# HOSTNAME = "localhost"
+HOSTNAME = "localhost"
+PORT = 15000
+
+
+class Signer(Initiator):
+
+    def __init__(self, group_init, address: str, port: int) -> None:
+        super().__init__(address, port)
+        self.g = G2.hashAndMapTo(group_init)
+        self.x = Fr()
+
+        # Generate public key
+        self.x.setByCSPRNG()
+        self.X = self.g * self.x
+
+    def send_public_key(self):
+        """Return X = g^x which is the public key"""
+        return self.X
+
+    def sign(self, m: str):
+        "Create signed message S = h ^ x"
+        h = G1.hashAndMapTo(m.encode())
+        S = h * self.x
+        return S
+
+
+if __name__ == "__main__":
+    signer = Signer(group_init, HOSTNAME, PORT)
+
+    X = signer.send_public_key()
+    m = "Bardzo tajna wiadomosc"
+
+    S = signer.sign(m)
+    signer.send_message(message=jstore({"X": X, "m": m, "S": S}))
--- BLS/verifier.py
+++ BLS/verifier.py
@@ -1,10 +1,10 @@
-from mcl import G2, Fr
+from mcl import G1, G2, GT
 import sys
 from common_protocol import Responder
 from klib import jload
 
 sys.path.insert(1, "/home/karolina/mcl-python")
-group_init = b"schnorr_ss"
+group_init = b"bls_ss"
 # HOSTNAME = "localhost"
 HOSTNAME = "localhost"
 PORT = 15000
@@ -16,10 +16,10 @@ class Verifier(Responder):
         super().__init__(address, port)
         self.g = G2.hashAndMapTo(group_init)
 
-    def verify(self, A, X, m: str, s):
+    def verify(self, X, m: str, S):
         """Verify the signature using pairing function."""
-        h = Fr.setHashOf((str(X) + m).encode())
-        if self.g * s == X + (A*h):
+        h = G1.hashAndMapTo(m.encode())
+        if GT.pairing(S, self.g) == GT.pairing(h, X):
             print("Accepted")
         else:
             print("Rejected")
@@ -28,6 +28,6 @@ class Verifier(Responder):
 if __name__ == "__main__":
     verifier = Verifier(group_init, HOSTNAME, PORT)
     m1 = verifier.receive_message()
-    A, X, m, s = jload({"A": G2, "X": G2, "m": str, "s": Fr}, m1)
+    X, m, S = jload({"X": G2, "m": str, "S": G1}, m1)
 
-    verifier.verify(A, X, m, s)
+    verifier.verify(X, m, S)
